28d38254617c8b910eb33d446f98fc34f5ab11f8
Marlon Emmerich <marlonemmerich@yahoo.com>
2021-08-11 21:33:20 -0300

    feat: Criaçao dos componentes base e roteamento

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CoreModule } from './src/core/core.module';
+import { SharedModule } from './src/shared/shared.module';
+import { AuthModule } from './src/auth/auth.module';
+import { ItensModule } from './src/itens/itens.module';
+import { RouterModule } from '@angular/router';
 @NgModule({
   declarations: [	
     AppComponent,
+
    ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CoreModule,
+    SharedModule,
+    AuthModule,
+    ItensModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/src/app-routing.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: 'login',
+    loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)
+  },
+  {
+    path: 'itens',
+    loadChildren: () => import('./itens/itens.module').then(m => m.ItensModule)
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes, {
+    // preload all modules; optionally we could
+    // implement a custom preloading strategy for just some
+    // of the modules (PRs welcome 😉)
+    preloadingStrategy: PreloadAllModules,
+    relativeLinkResolution: 'legacy'
+})],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
