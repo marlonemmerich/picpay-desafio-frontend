e53d5c072cc61bd5f6f48e9d317a3c717045d5f3
Marlon Emmerich <marlonemmerich@yahoo.com>
2021-08-18 22:53:00 -0300

    refactor: Alteraçoes para permitir receber parametro na construçao

--- src/app/pagamentos/models/date-pickup.ts
+++ src/app/pagamentos/models/date-pickup.ts
@@ -1,4 +1,8 @@
 export class DatePickup {
-    data = '';
-    hora = '';
+    data: string;
+    hora: string;
+    constructor(datePickup?: any) {
+        this.data = (datePickup && datePickup.data) ? datePickup.data : '';
+        this.hora = (datePickup && datePickup.hora) ? datePickup.hora : '';
+    }
 }
--- src/app/pagamentos/models/pagamento.model.ts
+++ src/app/pagamentos/models/pagamento.model.ts
@@ -7,4 +7,14 @@ export default class Pagamento {
     date: Date | string = '';
     image = '';
     isPayed = false;
+    constructor(pagamento? : any) {
+        this.id = (pagamento && pagamento.id) ? pagamento.id : null;
+        this.name = (pagamento && pagamento.name) ? pagamento.name : '';
+        this.username = (pagamento && pagamento.username) ? pagamento.username : '';
+        this.title  = (pagamento && pagamento.title) ? pagamento.title : '';
+        this.value = (pagamento && pagamento.value) ? pagamento.value : undefined;
+        this.date = (pagamento && pagamento.date) ? pagamento.date : '';
+        this.image = (pagamento && pagamento.image) ? pagamento.image : '';
+        this.isPayed = (pagamento && pagamento.isPayed) ? pagamento.isPayed : false;
+    }
 }
